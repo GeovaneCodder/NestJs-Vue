35a93f73e67cdbe364ca6ab72ace243aad547a9f
Geovane Souza <geovane@MacBook-Pro-de-Geovane.local>
2026-06-11 22:51:23 -0300

    Change class to interface

--- src/users/dto/user_dto.ts
+++ src/users/dto/user_dto.ts
@@ -1,12 +1,9 @@
-export class UserDto {
-
-    constructor(
-        public id: number,
-        public name: string,
-        public email: string,
-        public address: Record<string, any>,
-        public phone: string,
-        public website: string,
-        public company: Record<string, any>,
-    ) {}
+export interface UserDto {
+    id: number;
+    name: string;
+    email: string;
+    address: Record<string, any>;
+    phone: string;
+    website: string;
+    company: Record<string, any>;
 }
